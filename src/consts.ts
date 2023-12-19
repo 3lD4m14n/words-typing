fbd222f824a61318dda28ca323a3e4dc631479eb
Derlis Damian Meza Carballo <derlisdamianmeza@gmail.com>
2023-12-19 11:47:27 -0300

    deleted API_URL consts
    this change is because the use of websockets, then the URL to the websocket is storage in the enviroment

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,3 @@ export const SUCCESS_COLOR = "green";
 export const ERROR_COLOR = "red";
 export const MAX_LETTERS = 10;
 export const MIN_LETTERS = 1;
-
-//routes
-export const API_URL = "/api";
-export const API_GET_WORD = `${API_URL}/getword`;
